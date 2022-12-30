6bdf834d80ecfe4a42b0033e2fa946b31108c491
TheKoke <damirkoke01@gmail.com>
2022-12-31 03:19:04 +0600

    Final additions

--- prequel/molecule.py
+++ prequel/molecule.py
@@ -26,7 +26,7 @@ class Molecule(object):
                 else: picture += element + f'{count}'
 
         return picture
-    
+
     @property
     def atoms(self) -> list[Atom]:
         if not self.locked:
@@ -51,8 +51,8 @@ class Molecule(object):
         if not self.locked:
             raise UnlockedMolecule('Molecule must be locked before get them mass')
 
-        return sum([list(self.atoms)[i].get_weight() for i in range(len(list(self.atoms)))])
-    
+        return sum(self.atoms[i].get_weight() for i in range(len(self.atoms)))
+
     def brancher(self, *args: list[int]) -> Molecule:
         if self.locked:
             raise LockedMolecule('Molecule must be unlocked before change them')
@@ -65,7 +65,7 @@ class Molecule(object):
     def bounder(self, *args: list[tuple[int]]) -> Molecule:
         if self.locked:
             raise LockedMolecule('Molecule must be unlocked before change them')
-        
+
         for i in range(len(args)):
             c1: int = args[i][0]; b1: int = args[i][1]
             c2: int = args[i][2]; b2: int = args[i][3]
@@ -76,7 +76,7 @@ class Molecule(object):
     def mutate(self, *args: list[tuple[int]]) -> Molecule:
         if self.locked:
             raise LockedMolecule('Molecule must be unlocked before change them')
-        
+
         for i in range(len(args)):
             nc: int = args[i][0]; nb: int = args[i][1]; elt: str = args[i][2]
 
@@ -85,13 +85,36 @@ class Molecule(object):
     def add(self, nc: int, nb: int, elt: str) -> None:
         if self.locked:
             raise LockedMolecule('Molecule must be unlocked before change them')
-        
+
+        pretend = Atom(elt, 1)
+
+        if self.__branchers[nb][nc].add_neighrs(pretend):
+            self.atoms.append(pretend)
+
         return self
 
     def add_chaining(self, nc: int, nb: int, *args: list[str]) -> None:
         if self.locked:
             raise LockedMolecule('Molecule must be unlocked before change them')
-        
+
+        chain = [Atom(args[i], 1) for i in range(len(args))]
+
+        if len(chain) == 1:
+            if self.__branchers[nb][nc].add_neighrs(chain[0]):
+                self.atoms.append(chain[0])
+            return
+
+        for i in range(len(chain)):
+            if i == 0:
+                chain[i].add_neighrs(chain[i + 1])
+                continue
+
+            if i == len(chain) - 1:
+                chain[i].add_neighrs(chain[i - 1])
+                continue
+
+            chain[i].add_neighrs(chain[i + 1])
+            chain[i].add_neighrs(chain[i - 1])
         return self
 
     def closer(self) -> Molecule:
@@ -125,7 +148,7 @@ class Molecule(object):
         for i in range(len(self.__branchers[-1])):
             if i == 0:
                 self.__branchers[-1][i].add_neighrs(self.__branchers[-1][i + 1])
-            
+
             if i == len(self.__branchers[-1]) - 1:
                 self.__branchers[-1][i].add_neighrs(self.__branchers[-1][i - 1])
 
@@ -151,17 +174,18 @@ class Molecule(object):
                     self.__branchers[i][j].neighrs.remove(hydrogen)
                     self.__branchers[i].remove(hydrogen)
 
-    def __merger(self, first_branch: int, second_branch: int, pos1: int, pos2: int) -> bool:
-        # if elt1 >= len(self.atoms) or elt2 >= len(another.atoms):
-        #    return False
+    def __merger(self, branch1: int, branch2: int, pos1: int, pos2: int) -> bool:
+        if pos1 >= len(self.__branchers[branch1]) or pos2 >= len(self.__branchers[branch2]):
+           return False
 
-        # if len(self.atoms[elt1].neighrs) == self.atoms[elt1].get_valence() \
-        # or len(another.atoms[elt2].neighrs) == another.atoms[elt2].get_valence():
-        #    return False
+        if len(self.__branchers[branch1][pos1].neighrs) == self.__branchers[branch1][pos1].get_valence() \
+        or len(self.__branchers[branch2][pos2].neighrs) == self.__branchers[branch2][pos2].get_valence():
+           return False
 
-        # self.atoms[elt1].add_neighrs(Atom(another.atoms[elt2], len(another.atoms) + 1))
-        # another.atoms[elt2].add_neighrs(Atom(self.atoms[elt1], len(self.atoms) + 1))
-        pass
+        self.__branchers[branch1][pos1].add_neighrs(self.__branchers[branch2][pos2])
+        self.__branchers[branch2][pos2].add_neighrs(self.__branchers[branch1][pos1])
+        
+        return False
 
     def __count_atoms(self) -> dict[str, int]:
         res = dict()
@@ -171,5 +195,5 @@ class Molecule(object):
                 res[atom.element] += 1
             else:
                 res[atom.element] = 1
-        
+
         return res
